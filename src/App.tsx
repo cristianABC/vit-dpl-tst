4015a4e7588745c6d2ba29356bd6bc4c78ea0bd3
cristian <cristianarangodaza@gmail.com>
2024-09-24 07:10:46 -0500

    first

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Outlet, useNavigate } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+import './App.css'
+import { Toaster } from './components/ui/toaster'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+function App() {
+  const navigate = useNavigate();
+  return (
+    <ClerkProvider
+    routerPush={(to) => navigate(to)}
+    routerReplace={(to) => navigate(to, { replace: true })}
+    publishableKey={PUBLISHABLE_KEY}
+  > 
+  <header className="header">
+        <div style={{display: 'flex', justifyContent:'space-between'}}>
+          <div>
+            <p>Clad Logistics</p>
+          </div>
+          {/* <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <Link to="/sign-in">Iniciar sesión</Link>
+          </SignedOut> */}
+        </div>
+      </header>
+  <main>
+    <Outlet/>
+    <Toaster />
+  </main>
+  </ClerkProvider>
+  
+  )
+}
+
+export default App
